8a540b9943534404ca8c4e3fa71d031746052bae
unknown <rodrigueskenneth01@gmail.com>
2026-01-19 14:58:19 -0500

    Login: reemplazar fondo por SVG de alto contraste (tech-contrast.svg), tamaño 240%, opacidad 0.80 y velo más tenue + cache bust (login-art-6)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
             Versionado CSS para evitar caché del navegador
             (si cambian estilos, agrega un sufijo distinto)
             ----------------------------------- -->
-        <link href="{{ asset('css/style.css') }}?v=login-art-5" rel="stylesheet">
+        <link href="{{ asset('css/style.css') }}?v=login-art-6" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 </head>
 <body>
